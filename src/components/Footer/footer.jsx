f17c73eed46403f7c4c831325af930a1316235fc
Анастасия <n060600@yandex.ru>
2022-12-20 02:33:36 +0500

    test

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
     return (
-        <footer className='footer'>
+        <footer className='footer section'>
             <h4 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h4>
             <div className='footer__block'>
-                <p className='footer__year'>@2023</p>
+                <p className='footer__year'>&copy;2023</p>
                 <div className='footer__block-links'>
                     <a href='https://practicum.yandex.ru/' className='link footer__link' target='blank'>Яндекс.Практикум</a>
                     <a href='https://github.com/' className='link footer__link' target='blank'>Github</a>
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,12 +1,13 @@
 import logo from '../../images/logo.svg';
 import person from '../../images/person.svg';
+import white from '../../images/acc_white.svg'
 import { Link, NavLink } from 'react-router-dom';
 import { Fragment } from 'react';
 import Burger from '../Main/Burger/Burger';
 
 function Header({ loggedIn, movies }) {
     return (
-        <header className={`header ${movies && 'section_white'}`}>
+        <header className={`header ${movies && 'header_white'}`}>
             <div className='header__layout'>
                 <Link className='button logo-btn' to='/'>
                     <img src={logo} alt='Логотип.' className='logo' />
@@ -16,10 +17,10 @@ function Header({ loggedIn, movies }) {
                 {loggedIn ? (
                     <Fragment>
                         <div className='header__block-links'>
-                            <NavLink className="header__link link header__link_for_all-movies" activeClassName="header__link link header__link_active" to='/movies' exact>Фильмы</NavLink>
-                            <NavLink className="header__link link header__link_for_add-movies" activeClassName="header__link link header__link_active" to='/saved-movies' exact>Сохранённые фильмы</NavLink>
+                            <NavLink className={`header__link link header__link_for_all-movies ${!movies && 'link_white'}`} activeClassName='header__link link header__link_active'  to='/movies' exact>Фильмы</NavLink>
+                            <NavLink className={`header__link link header__link_for_add-movies ${!movies && 'link_white'}`} activeClassName='header__link link header__link_active' to='/saved-movies' exact>Сохранённые фильмы</NavLink>
                         </div>
-                        <Link className='account-button header__account link' to='/profile'><img src={person} alt='Аккаунт.' className='account-img' />Аккаунт</Link>
+                        <Link className={`account-button header__account link ${!movies && 'account-button_white link_white'}`} to='/profile'><img src={`${movies ? person : white}`} alt='Аккаунт.' className='account-img' />Аккаунт</Link>
                         <Burger />
                     </Fragment>
                 ) : (
