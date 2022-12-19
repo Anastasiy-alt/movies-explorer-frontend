9632f07df0cd29f89572d59d065efeaddd001b9f
Анастасия <n060600@yandex.ru>
2022-12-20 02:35:16 +0500

    Update promo.jsx

--- src/components/Main/Promo/promo.jsx
+++ src/components/Main/Promo/promo.jsx
@@ -4,7 +4,7 @@ function Promo() {
     return (
         <section className='promo'>
             <div className='promo__layout'>
-                <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+                <h1 className='promo__title'>Учебный&nbsp;проект студента факультета Веб&#8209;разработки.</h1>
                 <p className='promo__text'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                 <button className='button promo__learn-more'>Узнать больше</button>
             </div>
